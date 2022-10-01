110b939c0ca9998c4302bd29bca6c96421409cc6
emsleyd <emsleyd@gmail.com>
2022-10-01 18:14:20 +0100

    commit after having migrated changes to pgadmin

--- main_app/models.py
+++ main_app/models.py
@@ -7,18 +7,19 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to = 'main_app/static/uploads/', default="")
     description = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=300)
+    category = models.CharField(max_length=100, default="")
+    ingredients = models.CharField(max_length=300, default="")
     # method coule be potential image upload?
-    method = models.CharField(max_length=300)
-    portions = models.SmallIntegerField()
+    method = models.CharField(max_length=300, default="")
+    # need to find default value for portions
+    # portions = models.SmallIntegerField(default="4")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # option to make public or keep private?
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default="")
-    allergy = models.CharField(max_length=100)
+    allergy = models.CharField(max_length=100, default="")
 
 class User(models.Model):
     name = models.CharField(max_length=100)
