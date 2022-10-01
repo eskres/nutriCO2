972ff4a16d9a52d9e43c90f4b6bf4fb1505e3113
emsleyd <emsleyd@gmail.com>
2022-10-01 13:45:51 +0100

    CRUD added for Recipe and ingredient

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,9 +5,22 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    #RECIPES
     path('recipes/', views.recipes_index, name='index'),
     path('recipes/<int:recipe_id>', views.recipes_detail, name='detail'),
+    path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
+    path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipes_update'),
+    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
+
+    #INGREDIENTS
     path('ingredients/', views.ingredients_index, name='index'),
-    path('ingredients/<int:ingredient_id>', views.ingredients_detail, name='detail')
+    path('ingredients/<int:ingredient_id>', views.ingredients_detail, name='detail'),
+    path('ingredients/create/', views.IngredientCreate.as_view(), name='ingredients_create'),
+    path('ingredients/<int:pk>/update', views.IngredientUpdate.as_view(), name='ingredients_update'),
+    path('ingredients/<int:pk>/delete', views.IngredientDelete.as_view(), name='ingredients_delete'),
+
+    #SIGN UP
+    path('accounts/signup/', views.signup, name='signup')
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,33 @@
 from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Recipe, Ingredient, User
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
+
+#RECIPES CRUD
+class RecipeCreate(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class RecipeUpdate(LoginRequiredMixin, UpdateView):
+    model = Recipe
+    fields = ['name']
+
+class RecipeDelete(LoginRequiredMixin, DeleteView):
+    model = Recipe
+    success_url = '/recipes/'
 
 
 def home(request):
@@ -21,6 +44,27 @@ def recipes_detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     return render(request, 'recipes/detail.html', {'recipe': recipe})
 
+#INGREDIENTS CRUD
+class IngredientList(LoginRequiredMixin, ListView):
+    model = Ingredient
+
+class IngredientDetail(LoginRequiredMixin, DetailView):
+    model = Ingredient
+
+class IngredientCreate(LoginRequiredMixin, CreateView):
+    model = Ingredient
+    fields = '__all__'
+
+class IngredientUpdate(LoginRequiredMixin, UpdateView):
+    model = Ingredient
+    fields = '__all__'
+
+class IngredientDelete(LoginRequiredMixin, DeleteView):
+    model = Ingredient
+    success_url = '/ingredients/'
+
+
+
 def ingredients_index(request):
     ingredients = Ingredient.objects.all()
     return render(request, 'ingredients/index.html', { 'ingredients': ingredients})
@@ -28,3 +72,15 @@ def ingredients_index(request):
 def ingredients_detail(request, ingredient_id):
     ingredient = Ingredient.objects.get(id=ingredient_id)
     return render(request, 'ingredients/detail.html', { 'ingredients': ingredient})
+
+def signup(request):
+    error_message =""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = "Invalid signup"
