9ddc0b8c5af275fea731c596d63ad1678c798ce0
eskres <einar.skreslett@gmail.com>
2022-10-04 15:55:48 +0100

    image to text api functioning

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,4 +2,5 @@ from dataclasses import field
 from django import forms
 
 class ImageTextForm(forms.Form):
-    image = forms.ImageField()
+    image = forms.ImageField(required=False)
+    text = forms.CharField(label='Text Response', max_length=100, required=False)
